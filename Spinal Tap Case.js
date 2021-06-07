93d56a6f183736cfb6b48a28659bf4f132724b10
kitoshi <kitoshi.charlton@gmail.com>
2021-06-07 15:05:23 -0700

    added url to spinal tap

--- Spinal Tap Case.js
+++ Spinal Tap Case.js
@@ -21,5 +21,6 @@ function spinalCase(str) {
   Spinal Tap Case
 
 Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/spinal-tap-case
 
   */
