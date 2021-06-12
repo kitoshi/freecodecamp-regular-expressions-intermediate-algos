4c231112d1a39630efb915987be5eb579eab819b
kitoshi <kitoshi.charlton@gmail.com>
2021-06-11 22:54:31 -0700

    decimal working for change

--- Cash Register.js
+++ Cash Register.js
@@ -17,7 +17,7 @@ function checkCashRegister(price, cash, cid) {
   var decimal = parseFloat((change - Math.floor(change)).toPrecision(2))
   var returnchange = Math.trunc(change)
   const reversed = cid.reverse() //100 to penny actual register
-  do {
+  /*do {
     switch (true) {
       case (returnchange >= 100):
         var output = Math.floor(returnchange / reversedLegend[0][1])
@@ -35,28 +35,53 @@ function checkCashRegister(price, cash, cid) {
         var returnchange = (returnchange - (output * 10)).toFixed(2)
         break;
       case (returnchange >= 5 && returnchange < 10):
-        console.log(returnchange)
-        var output = (returnchange / reversedLegend[3][1])
+        var output = Math.floor(returnchange / reversedLegend[3][1])
         newArr.push(reversed[3][0], output * reversedLegend[3][1])
         var returnchange = (returnchange - (output * 5)).toFixed(2)
         break;
-      case (returnchange > 0 && returnchange < 5):
-        var output = (returnchange / reversedLegend[4][1])
+      case (returnchange >= 1 && returnchange < 5):
+        var output = Math.floor(returnchange / reversedLegend[4][1])
         newArr.push(reversed[4][0], output * reversedLegend[4][1])
         var returnchange = (returnchange - (output * 1)).toFixed(2)
         break;
       default:
     }
-  } while (returnchange > 0) {
+  } while (returnchange > 0) */
+  do {
+    switch (true) {
+      case (decimal < 1 && decimal >= 0.25):
+        var output = Math.floor(decimal / reversedLegend[5][1])
+        newArr.push(reversed[5][0], output * reversedLegend[5][1])
+        var decimal = (decimal - (output * 0.25)).toFixed(2)
+        break;
+     case (decimal >= 0.1 && decimal < 0.25):
+        var output = Math.floor(decimal / reversedLegend[6][1])
+        newArr.push(reversed[6][0], output * reversedLegend[6][1])
+        var decimal = (decimal - (output * 0.10)).toFixed(2)
+        break;
+     case (decimal >= 0.05 && decimal < 0.1):
+        var output = Math.floor(decimal / reversedLegend[7][1])
+        newArr.push(reversed[7][0], output * reversedLegend[7][1])
+        var decimal = (decimal - (output * 0.05)).toFixed(2)
+        break;
+     case (decimal >= 0.01 && decimal < 0.05):
+        console.log(decimal)
+        var output = Math.floor(decimal / reversedLegend[8][1])
+        newArr.push(reversed[8][0], output * reversedLegend[8][1])
+        var decimal = (decimal - (output * 0.01)).toFixed(2)
+        break;
+      default:
+    }
+  } while (decimal > 0.00)
+  {
     const opendrawer = { status: "OPEN", change: [newArr] }
-    console.log(reversed['TWENTY'])
-    /*
     //testing if cash register has enough in bills
-      if (newArr[str][1] > reversed[str][1]) {
+      if (false) {
         const bankrupt = { status: "INSUFFICIENT_FUNDS", change: [] }
         return Object.assign(drawer, bankrupt)
       } else {
-        */
+        console.log(opendrawer)
+
         return Object.assign(drawer, opendrawer);
       }
     }
@@ -67,4 +92,4 @@ function checkCashRegister(price, cash, cid) {
   
 
 
-checkCashRegister(1, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 300]])
+checkCashRegister(1.49, 2, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 300]])
