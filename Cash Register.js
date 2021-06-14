5129de24194b22d2822c23489c7b9e70da5ba9a1
kitoshi <kitoshi.charlton@gmail.com>
2021-06-14 11:41:15 -0700

    cash register completed

--- Cash Register.js
+++ Cash Register.js
@@ -12,11 +12,29 @@ function checkCashRegister(price, cash, cid) {
     ["ONE HUNDRED", 100]
   ]
   const reversedLegend = number.reverse() //legend
-  const drawer = { status: "CLOSED", change: cid }
+  const drawer = {status: "CLOSED", change: cid}
   var change = cash - price
   var decimal = parseFloat((change - Math.floor(change)).toPrecision(2))
   var returnchange = Math.trunc(change)
-  const reversed = cid.reverse() //100 to penny actual register
+  var totalcidwhole = 0
+  var decimalcidwhole = 0
+  const reversed = cid.reverse()
+  for (let i = 0; i < 4; i++) {
+    totalcidwhole += reversed[i][1]
+  }
+  for (let k = 5; reversed.length > k; k++){
+    decimalcidwhole += reversed[k][1]
+  }
+  if ((returnchange + decimal) == (decimalcidwhole + totalcidwhole)){
+    cid.reverse()
+    console.log('exact')
+    return drawer
+  } else if (totalcidwhole < returnchange || decimalcidwhole < decimal) {
+    const bankrupt = {status: "INSUFFICIENT_FUNDS", change: []}
+    console.log('bankrupt')
+    return Object.assign(drawer, bankrupt)
+    } else {
+   //100 to penny actual register
   do {
     switch (true) {
       case (returnchange >= 100):
@@ -25,53 +43,54 @@ function checkCashRegister(price, cash, cid) {
         var returnchange = (returnchange - (output * 100)).toFixed(2)
         break;
       case (returnchange >= 20 && reversed[1][1] >= 20 && newArr.includes('TWENTY') == false):
-      console.log(returnchange)
         var output = Math.floor(returnchange / reversedLegend[1][1])
         if (reversed[1][1] < output * reversedLegend[1][1]) {
           var remainder = returnchange - output * reversedLegend[1][1] // remainder needs to be
           //in all combos
-          console.log(remainder)
           var difference = output * reversedLegend[1][1] - reversed[1][1]
           newArr.push(reversed[1][0], reversed[1][1])
           var returnchange = difference + remainder // remainder added here
           break;
-        } else if (newArr.includes('TWENTY') == false){
+        } else if (newArr.includes("TWENTY") == false){
         newArr.push(reversed[1][0], output * reversedLegend[1][1])
         var returnchange = (returnchange - (output * 20)).toFixed(2)
         break;
         }
-     case (returnchange >= 10 && reversed[2][1] >= 10 && newArr.includes('TEN') == false):
+     case (returnchange >= 10 && reversed[2][1] >= 10 && newArr.includes("TEN") == false):
      var output = Math.floor(returnchange / reversedLegend[2][1])
         if (reversed[2][1] < output * reversedLegend[2][1]) {
+          var remainder = returnchange - output * reversedLegend[2][1]
           var difference = output * reversedLegend[2][1] - reversed[2][1]
           newArr.push(reversed[2][0], reversed[2][1])
-          var returnchange = difference
+          var returnchange = difference + remainder
           break;
-        } else if (newArr.includes('TEN') == false){
+        } else if (newArr.includes("TEN") == false){
         newArr.push(reversed[2][0], output * reversedLegend[2][1])
         var returnchange = (returnchange - (output * 10)).toFixed(2)
         break;
         }
-     case (returnchange >= 5 && reversed[3][1] >= 5 && newArr.includes('FIVE') == false):
+     case (returnchange >= 5 && reversed[3][1] >= 5 && newArr.includes("FIVE") == false):
      var output = Math.floor(returnchange / reversedLegend[3][1])
         if (reversed[3][1] < output * reversedLegend[3][1]) {
+          var remainder = returnchange - output * reversedLegend[3][1]
           var difference = output * reversedLegend[3][1] - reversed[3][1]
           newArr.push(reversed[3][0], reversed[3][1])
-          var returnchange = difference
+          var returnchange = difference + remainder
           break;
-        } else if (newArr.includes('FIVE') == false){
+        } else if (newArr.includes("FIVE") == false){
         newArr.push(reversed[3][0], output * reversedLegend[3][1])
         var returnchange = (returnchange - (output * 5)).toFixed(2)
         break;
         }
-       case (returnchange >= 1 && reversed[4][1] >= 1 && newArr.includes('ONE') == false):
+       case (returnchange >= 1 && reversed[4][1] >= 1 && newArr.includes("ONE") == false):
        var output = Math.floor(returnchange / reversedLegend[4][1])
         if (reversed[4][1] < output * reversedLegend[4][1]) {
+          var remainder = returnchange - output * reversedLegend[4][1]
           var difference = output * reversedLegend[4][1] - reversed[4][1]
           newArr.push(reversed[4][0], reversed[4][1])
-          var returnchange = difference
+          var returnchange = difference + remainder
           break;
-        } else if (newArr.includes('ONE') == false){
+        } else if (newArr.includes("ONE") == false){
         newArr.push(reversed[4][0], output * reversedLegend[4][1])
         var returnchange = (returnchange - (output * 1)).toFixed(2)
         break;
@@ -81,74 +100,84 @@ function checkCashRegister(price, cash, cid) {
   } while (returnchange > 0) 
   do {
     switch (true) {
-      case (decimal >= 0.25 && reversed[5][1] >= 0.25 && newArr.includes('QUARTER') == false):
-        var output = Math.floor(decimal / reversedLegend[5][1])
-        if (reversed[5][1] < output * reversedLegend[5][1]) {
-        var difference = output * reversedLegend[5][1] - reversed[5][1]
+      case (decimal >= 0.25 && reversed[5][1] >= 0.25 && newArr.includes("QUARTER") == false):
+        var decoutput = Math.floor(decimal / reversedLegend[5][1])
+        if (reversed[5][1] < decoutput * reversedLegend[5][1]) {
+        var decremainder = decimal - decoutput * reversedLegend[5][1]
+        var difference = decoutput * reversedLegend[5][1] - reversed[5][1]
         newArr.push(reversed[5][0], reversed[5][1])
-        var decimal = difference
+        var decimal = difference + decremainder
         break;
-        } else if (newArr.includes('QUARTER') == false){
-        newArr.push(reversed[5][0], output * reversedLegend[5][1])
-        var decimal = (decimal - (output * 0.25)).toFixed(2)
+        } else if (newArr.includes("QUARTER") == false){
+        newArr.push(reversed[5][0], decoutput * reversedLegend[5][1])
+        var decimal = (decimal - (decoutput * 0.25)).toFixed(2)
         break;
         }
-      case (decimal >= 0.1 && reversed[6][1] >= 0.1 && newArr.includes('DIME') == false):
-        var output = Math.floor(decimal / reversedLegend[6][1])
-        if (reversed[6][1] < output * reversedLegend[6][1]) {
-        var difference = output * reversedLegend[6][1] - reversed[6][1]
+      case (decimal >= 0.1 && reversed[6][1] >= 0.1 && newArr.includes("DIME") == false):
+        var decoutput = Math.floor(decimal / reversedLegend[6][1])
+        if (reversed[6][1] < decoutput * reversedLegend[6][1]) {
+        var decremainder = decimal - decoutput * reversedLegend[6][1]
+        var difference = decoutput * reversedLegend[6][1] - reversed[6][1]
         newArr.push(reversed[6][0], reversed[6][1])
-        var decimal = difference
+        var decimal = difference + decremainder
         break;
-        } else if (newArr.includes('DIME') == false){
-        newArr.push(reversed[6][0], output * reversedLegend[6][1])
-        var decimal = (decimal - (output * 0.10)).toFixed(2)
+        } else if (newArr.includes("DIME") == false){
+        newArr.push(reversed[6][0], decoutput * reversedLegend[6][1])
+        var decimal = (decimal - (decoutput * 0.10)).toFixed(2)
         break
         }
-      case (decimal >= 0.05 && reversed[7][1] >= 0.05 && newArr.includes('NICKEL') == false):
-        var output = Math.floor(decimal / reversedLegend[7][1])
-        if (reversed[7][1] < output * reversedLegend[7][1]) {
-        var difference = output * reversedLegend[7][1] - reversed[7][1]
+      case (decimal >= 0.05 && reversed[7][1] >= 0.05 && newArr.includes("NICKEL") == false):
+        var decoutput = Math.floor(decimal / reversedLegend[7][1])
+        if (reversed[7][1] < decoutput * reversedLegend[7][1]) {
+        var decremainder = decimal - decoutput * reversedLegend[7][1]
+        var difference = decoutput * reversedLegend[7][1] - reversed[7][1]
         newArr.push(reversed[7][0], reversed[7][1])
-        var decimal = difference
+        var decimal = difference + decremainder
         break;
-        } else if (newArr.includes('NICKEL') == false){
-        newArr.push(reversed[7][0], output * reversedLegend[7][1])
-        var decimal = (decimal - (output * 0.05)).toFixed(2)
+        } else if (newArr.includes("NICKEL") == false){
+        newArr.push(reversed[7][0], decoutput * reversedLegend[7][1])
+        var decimal = (decimal - (decoutput * 0.05)).toFixed(2)
         break
         }
-    case (decimal >= 0.01 && reversed[8][1] >= 0.01 && newArr.includes('PENNY') == false):
-        var output = Math.floor(decimal / reversedLegend[8][1])
-        if (reversed[8][1] < output * reversedLegend[8][1]) {
-        var difference = output * reversedLegend[8][1] - reversed[8][1]
+    case (decimal >= 0.01 && reversed[8][1] >= 0.01 && newArr.includes("PENNY") == false):
+        var decoutput = Math.floor(decimal / reversedLegend[8][1])
+        if (reversed[8][1] < decoutput * reversedLegend[8][1]) {
+        var difference = decoutput * reversedLegend[8][1] - reversed[8][1]
         newArr.push(reversed[8][0], reversed[8][1])
         var decimal = difference
         break;
-        } else if (newArr.includes('PENNY') == false){
-        newArr.push(reversed[8][0], output * reversedLegend[8][1])
-        var decimal = (decimal - (output * 0.01)).toFixed(2)
+        } else if (newArr.includes("PENNY") == false){
+        newArr.push(reversed[8][0], decoutput * reversedLegend[8][1])
+        var decimal = (decimal - (decoutput * 0.01)).toFixed(2)
         break
         }
       default:
     }
   } while (decimal > 0.00)
-  {
-    const opendrawer = { status: "OPEN", change: [newArr] }
-    //testing if cash register has enough in bills
-      if (false) {
-        const bankrupt = { status: "INSUFFICIENT_FUNDS", change: [] }
-        return Object.assign(drawer, bankrupt)
-      } else {
-        console.log(opendrawer)
-
-        return Object.assign(drawer, opendrawer);
-      }
-    }
+  
+    
+    
+    
   }
 
+    const res = []; //formatting for answers
+    for (let i = 0; i < newArr.length; i += 2) {
+        const chunk = newArr.slice(i, i + 2);
+        res.push(chunk);
+    console.log(res)
+
+}
+
+     
+
+  const opendrawer = {status: "OPEN", change: res}
+  console.log(opendrawer)
+        return opendrawer;
+}
 
  
   
 
 
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
 checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
